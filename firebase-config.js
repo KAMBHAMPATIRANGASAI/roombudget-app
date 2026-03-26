b44a4c41a27760bb071d0bf62a06dab145e60a70
Kambhampati Ranga Sai <rangasaik06@gmail.com>
2026-03-26 22:22:56 +0530

    Firestore realtime sync

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5dCGErI4XJSm7bAXI0O3S6cNd9KX8pMw",
+  authDomain: "roombudget-1b3b4.firebaseapp.com",
+  projectId: "roombudget-1b3b4",
+  storageBucket: "roombudget-1b3b4.appspot.com",
+  messagingSenderId: "1018796101674",
+  appId: "1:1018796101674:web:4a2e9c3b0c4f2e9b2b8b5c"
+};
+
