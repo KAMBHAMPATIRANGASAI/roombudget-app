f1edea59cbed980fa2deb3034b00e87a76ff911e
Kambhampati Ranga Sai <rangasaik06@gmail.com>
2026-03-26 23:21:05 +0530

    fix firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD5dCGErI4XJSm7bAXI0O3S6cNd9KX8pMw",
-  authDomain: "roombudget-1b3b4.firebaseapp.com",
-  projectId: "roombudget-1b3b4",
-  storageBucket: "roombudget-1b3b4.appspot.com",
-  messagingSenderId: "1018796101674",
-  appId: "1:1018796101674:web:4a2e9c3b0c4f2e9b2b8b5c"
+  apiKey: "AIzaSyC7yj0KCcpyb1lZZnzp4LKfZBIeYKKvXOc",
+  authDomain: "roombudget-app.firebaseapp.com",
+  projectId: "roombudget-app",
+  storageBucket: "roombudget-app.firebasestorage.app",
+  messagingSenderId: "1056177026184",
+  appId: "1:1056177026184:web:53427486a28076270e5b3d"
 };
-
